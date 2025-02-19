d0c1ec4557a3a356d2065980802f2685fd9a5e5e
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-02-19 17:04:03 +0000

    214

---
+++ python.py
@@ -0,0 +1,16 @@
+nome = str(input ('Digite seu Nome'));
+edereco = str (input ('endereço'));
+cpf = float (input('numero cpf'));
+numero de telefone = float (('digite numero de telefone'));
+email = str (input('digite seu imail'));
+data_nascimento = (input('coleque sua data de nascimento  (DD/MM/AAAA):'));
+genero = (input('digite seu genero'));
+
+
+print(f''nome {nome}'' )
+print(f'' endereco {endereco}'')
+print (f'' numero de telefone {numero de telefone}'')
+print (f''cpf{cpf}'')
+print(f'' email{email}'')
+print(f''genero{genero}'')
+print(f''data_nascimento{data_nascimento}'')
